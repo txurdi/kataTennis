c329e0a043d87af19d05d8078c5bb3aebb3c5f66
txurdi <igor@txurdi.net>
2012-10-06 12:49:44 +0200

    Finalizamos la interaccion UpSide-Down

--- kataTennis.php
+++ kataTennis.php
@@ -6,7 +6,34 @@
  */
 
 class KataTennis {
+    private $puntosJugador1;
+    private $puntosJugador2;
+    private $ultimapuntuacion=false;
 
+    public function DameGanador() {
+        return '1';
+    }
+    
+    public function JuegoTerminado() {
+        if (($this->DameGanador()=='1') || ($this->DameGanador()=='2')) return true;
+        else return false;
+    }
+    
+    public function TomaPuntosJugador1($puntos) {
+        $this->puntosJugador1 = $puntos;
+    }
+    
+    public function TomaPuntosJugador2($puntos) {
+        $this->puntosJugador2 = $puntos;        
+    }
+    
+    public function PuntuaJugador1() {
+        $this->ultimapuntuacion = '1';
+    }
+    
+    public function PuntuaJugador2() {
+        $this->ultimapuntuacion = '2';
+    }
     
     
 }
--- tests/KataTennisTest.php
+++ tests/KataTennisTest.php
@@ -27,6 +27,29 @@ class KataTennisTest extends PHPUnit_Framework_TestCase
     {
     }
     
-    
-    
+    public function testJuegoSoloTerminaSiHayGanador() {
+        
+        $hayganador = (($this->object->DameGanador()=='1') || ($this->object->DameGanador()=='2'));                
+                
+        $this->assertEquals($hayganador,$this->object->JuegoTerminado());
+    }
+
+    public function testJugador1GanaSiPuntuaCon40yJugador2Menosde40() {
+        
+        $this->object->TomaPuntosJugador1('40');
+        $this->object->TomaPuntosJugador2('30');
+        $this->object->PuntuaJugador1();
+        
+        $this->assertEquals($this->object->DameGanador(),'1');
+    }
+
+    public function testJugador2GanaSiPuntuaCon40yJugador1Menosde40() {
+        
+        $this->object->TomaPuntosJugador1('30');
+        $this->object->TomaPuntosJugador2('40');
+        $this->object->PuntuaJugador2();
+        
+        $this->assertEquals($this->object->DameGanador(),'2');
+    }
+
 }
